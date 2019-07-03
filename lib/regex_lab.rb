683dcdfae636ba887c526e74acfc4a284e4a95af
Amy Fogelman <abfnj1@gmail.com>
2019-07-03 18:41:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,11 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+   if word.scan(/^[aeiouAEIOU]/).empty?
+    false
+  else
+    true
+  end
+end	end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
